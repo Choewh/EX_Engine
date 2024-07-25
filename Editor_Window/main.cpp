44473f28d38edd8976d168279fae29927209c1bb
dkqpqp2 <dkqpqp2@gmail.com>
2024-07-25 11:00:48 +0900

    7/25

--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -46,15 +46,43 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, // 프로그램의 인스턴스 
 
     MSG msg;
 
+    //(GetMessage(&msg, nullptr, 0, 0)
+    // 프로세스에서 발생한 메세지를 메세지 큐에서 가져오는 ㅅ함수
+    // 메세지큐에 아무것도 없다면??? 아무 메세지도 가져오지 않게된다
+
+    // PeekMessage : 메세지큐에 메세지 유무에 상관없이 함수가 리턴된다.
+    //                  리턴 값이 true인 경우 메세지가 있고 false인 경우는 메세지가 없다라고 가르켜준다.
+
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            int a = 0;
+            // 메세지가 없을 경우 여기서 처리
+            // 게임 로직이 들어가면 된다.
+        }
+    }
+
     // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    /*while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-    }
+    }*/
 
     return (int) msg.wParam;
 }
@@ -148,44 +176,48 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_PAINT:
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-
-
-            HBRUSH bluebrush = CreateSolidBrush(RGB(0, 0, 255));
-            HBRUSH oldbrush = (HBRUSH)SelectObject(hdc, bluebrush);
-
-            int i = 100;
-            int j = 200;
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
             
-            Ellipse(hdc, i, i, j, j);
-
-            (HBRUSH)SelectObject(hdc, oldbrush); //브러쉬 선택
-            DeleteObject(bluebrush);             //브러쉬 삭제
-
-            HPEN redPen = CreatePen(PS_SOLID, 5, (RGB(255, 0, 0)));
-
-            HPEN oldPen = (HPEN)SelectObject(hdc, redPen);
-
-            Ellipse(hdc, 300, 300, 400, 400);   
-            (HPEN)SelectObject(hdc, oldPen);     //브러쉬 선택 
-            DeleteObject(redPen);                //브러쉬 삭제 
-
-            //기본으로 자주사용 되는 GDI오브젝트를 미리 DC안에 만들어두었는데
-            // 그 오브젝트들을 스톡 오브젝트라고 한다.
-
-            //HBRUSH oldbrush = (HBRUSH)SelectObject(hdc, brush);
-
-            //Ellipse(hdc, 500, 500, 600, 600);
-
+            PAINTSTRUCT ps;
+           
             // DC란 화면에 출력에 필요한 모든 정보를 가지는 데이터 구조체이며
             // GDI모듈에 의해서 관리된다
             // 어떤 폰트를 사용할건가?, 어떤 선의 굵기를 정해줄건가 어떤 색상으로 그려줄껀가
             // 화면 출력에 필요한 모든 경우는 WINAPI에서는 DC를 통해서 작업을 진행할 수 있다.
+            
+            // 개인적인 생각
+            // HDC는 필통정도로 생각함 그리기툴.
+
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            HBRUSH bluebrush = CreateSolidBrush(RGB(0, 0, 255));        //핸들선언과 동시에 초기화
+            HBRUSH oldbrush = (HBRUSH)SelectObject(hdc, bluebrush);     //이전핸들 저장
+            Ellipse(hdc, 100,100,200,200);                              //브러쉬 사용
+            (HBRUSH)SelectObject(hdc, oldbrush);                        //이전브러쉬 되돌려놓기
+            DeleteObject(bluebrush);                                    //사용했던 브러쉬 삭제
+
+            HPEN redPen = CreatePen(PS_SOLID, 5, (RGB(255, 0, 0)));     //핸들선언과 동시에 초기화 
+            HPEN oldPen = (HPEN)SelectObject(hdc, redPen);              //이전핸들 저장
+            Ellipse(hdc, 300, 300, 400, 400);                           //펜 사용
+            (HPEN)SelectObject(hdc, oldPen);                            //이전브러쉬 되돌려놓기
+            DeleteObject(redPen);                                       //사용했던 브러쉬 삭제
+
+            // 스톡 오브젝트 (HBRUSH)GetStockObject( int Color );
+                                   /*""""""""""""""*/
+            //기본으로 자주사용 되는 GDI오브젝트를 미리 DC안에 만들어두었는데
+            // 그 오브젝트들을 스톡 오브젝트라고 한다.
+            HBRUSH graybrush = (HBRUSH)GetStockObject(GRAY_BRUSH);      //스톡오브젝트로 선언과 동시에 초기화
+                                     /*""""""""""""""*/
+            oldbrush = (HBRUSH)SelectObject(hdc, graybrush);            //이전핸들 저장
+            Rectangle(hdc, 100, 100, 300, 500);                         //브러쉬 사용
+            (HBRUSH)SelectObject(hdc, oldbrush);                        //이전브러쉬 되돌려놓기
+            DeleteObject(graybrush);                                    //사용했던 브러쉬 삭제
+            
+            
+            // 도형 ( 핸들 , 왼쪽 , 위 , 오른쪽 , 아래 )
             // 
-            // 
-            // 
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            // 그린 순서대로 덮어씌워짐 (마지막에 그린 그림이 맨위에 위치함)
+            
             EndPaint(hWnd, &ps);
         }
         break;
